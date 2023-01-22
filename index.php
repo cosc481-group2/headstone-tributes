ef3cc3c22cd173ee4bea4df78c4dfbfc10eea9f8
William Britton <kingnathanal@gmail.com>
2023-01-21 22:37:25 -0500

    doing a git check

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div>
+        <h1>Hello From Buffalo</h1>
+    </div>
 
-    echo("Hello World");
+    <?php
+
+    echo("Hello World/n");
 
     echo("Welcome to the fablous world of PHP");
 
-?>
+    ?>
+
+</body>
+</html>
