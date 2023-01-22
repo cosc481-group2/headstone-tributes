172cc3fbfeedb85dd91673d5eadf34bd701ce454
William Britton <kingnathanal@gmail.com>
2023-01-22 12:29:44 -0500

    commit new files

--- index.php
+++ index.php
@@ -4,21 +4,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>HeadStone-Tributes.com</title>
 </head>
 <body>
     
-    <div>
-        <h1>Hello From Buffalo</h1>
+    <div class="container-fluid text-center h-100">
+        <div class="row">
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">HeadStone-Tributes.com</span>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul class="navbar-nav me-auto mb-2">
+                            <li class="nav-item">
+                                <a href="#" class="nav-item active">Home</a>
+                            </li>
+                            <li class="nav-item">HeadStones</li>
+                            <li class="nav-item">Advice</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="row p-0">
+            <img src="img/pexels-koolshooters-6495720.jpg" alt="" class="img-fluid p-0 vh-100">
+        </div>
+        <div class="row"> 
+            <div class="p-3 col bg-secondary bg-gradient h-50"> column </div>
+            <div class="p-3 col bg-primary bg-gradient"> column </div>
+            <div class="p-3 col bg-success bg-gradient"> column </div> 
+        </div>
+        <div class="row">
+            <div class="container-md p-5">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            </div>
+        </div>
     </div>
 
-    <?php
-
-    echo("Hello World/n");
-
-    echo("Welcome to the fablous world of PHP");
-
-    ?>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
