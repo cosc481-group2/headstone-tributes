d559318943517993311c3ac25c1713094875ae9c
William Britton <kingnathanal@gmail.com>
2023-01-25 13:55:39 -0500

    test db con

--- config/dbtest.php
+++ config/dbtest.php
@@ -1,10 +1,10 @@
 <?php
 
-    $username = getenv('DB_USER');
-    $password = getenv('DB_PASS'); 
-    $dbName = getenv('DB_NAME');
-    $instanceHost = getenv('DB_HOST');
-    $instanceUnixSocket = getenv('INSTANCE_UNIX_SOCKET');
+    $username = "will"; 
+    $password = "pw";
+    $dbName = "testdb1"; 
+    $instanceHost = "34.162.157.82";
+    $instanceUnixSocket = '/cloudsql/proj-481:us-east5:w481';
 
     try {
 
@@ -18,18 +18,30 @@
             $dsn,
             $username,
             $password,
+            //[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ] // PDO::FETCH_CLASS FETCH_ASSC
         );
 
-        echo "Successfully connected to MYSQL database PDO";
+        echo "Successfully connected to MYSQL database PDO"."<br />";
 
-    } catch (PDOException $e) {
-            throw new RuntimeException(
+    } catch (\PDOException $e) {
+            throw new \PDOException(
                 sprintf(
-                    'Could not connect to the Cloud SQL Database',
+                    'Could not connect to the Cloud NSQL Database',
                     $e->getMessage()
                 ),
-                $e->getCode(),
-                $e
+                $e->getCode()
             );
     }
 
+    $data = $conn->query("SELECT * FROM users")->fetchAll();
+    // and somewhere later:
+    foreach ($data as $row) {
+        echo $row['user_name']." ".$row['last_name']."<br />";
+    }
+
+// $query = 'INSERT INTO users (email, full_name, is_active, created_at) VALUES (?,?,?,?)';
+// $query = 'INSERT INTO users (email, full_name, is_active, created_at) VALUES (:email, :name, :active, :date)';
+// $stmt = $db->prepare($query);
+// $stmt->execute([1,2,3,4]);
+
+// $id= (int) $db->lastInsertId();
