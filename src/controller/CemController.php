486d08f56f1a42938cd96e9694bc5a83a7bcf8ed
Mark Wholihan <mwholiha@emich.edu>
2023-02-04 16:17:46 -0500

    Added CemController.php and its associated functions

---
+++ src/controller/CemController.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once '../service/CemService.php';
+require_once '../model/Cemeteries.php';
+//require_once '../model/Login.php';
+
+use SERVICE\CemService;
+use MODEL\Cemeteries;
+//use MODEL\Login;
+
+
+function getAllCemeteries()
+{
+    $service = new cemService();
+    $cemeteries = $service->getCemeteries();
+    echo json_encode($cemeteries, JSON_PRETTY_PRINT);
+    return;
+}
+
+function getCemetery()
+{
+    $service = new cemService();
+    $cemetery = $service->getCemetery($_GET["cem_id"]);
+    echo json_encode($cemetery, JSON_PRETTY_PRINT);
+    return;
+}
+
+function addUser()
+{
+    //$login = new Login();
+    //$login->setUserId($_POST["user_id"]);
+    //$login->setPassword($_POST["pw"]);
+    //$login->setComments('');
+
+    $cemetery = new Cemeteries();
+    $cemetery->setCemId($_POST["cem_id"]);
+    $cemetery->setConId($_POST["con_id"]);
+    $cemetery->setCemName($_POST["cem_name"]);
+    $cemetery->setCemCity($_POST["cem_city"]);
+    $cemetery->setCemComments($_POST["cem_comments"]);
+
+    $service = new userService();
+    $service->addCemetery($cemetery, $login);
+    return;
+}
+
+function deleteCemetery()
+{
+    $service = new cemService();
+    $service->deleteCemetery($_POST["cem_id"]);
+    return;
+}
+
+# Check is FUNC param is set for either GET or POST and excute the value
+if(isset($_GET["func"]))
+{
+    $func = $_GET["func"];
+
+    switch($func) 
+    {
+        case 'all':
+            getAllCemeteries();
+            break;
+        case 'get':
+            getCemetery();
+            break;
+    }
+    
+} 
+else if (isset($_POST["func"])) 
+{
+    $func = $_POST["func"];
+
+    switch($func) 
+    {
+        case 'add':
+            addCemetery();
+            break;
+        case 'del':
+            deleteCemetery();
+            break;
+    }
+}
+
+?>
