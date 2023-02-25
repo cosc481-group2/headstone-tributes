dedcbad2d6115653ce95b26c5d7f04fc9717668d
chollens <105670047+chollens@users.noreply.github.com>
2023-02-25 00:42:51 -0500

    added navigation between different website pages

---
+++ create_new.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+ <a href="index.php" role="button" class = "btn btn-primary">Home</a>
+
+<div class="card w-50 mx-auto my-5">
+<div class="card-body">
+<form action=""> 
+    <div class="mt-3">
+        <label for="first_name" class="form-label">first name</label>
+        <input type="text" id = "first_name" class="form-control">
+        <label for="last_name" class="form-label">last name</label>
+        <input type="text" id = "last_name" class="form-control">
+        <div class="mt-3">
+        <label for="date_born" class="form-label">date of birth</label>
+        <input type="text" id = "date_born" class="form-control">
+        <div class="mt-3">
+        <label for="date_died" class="form-label">date of death</label>
+        <input type="text" id = "date_died" class="form-control">
+        <div class="mt-3">
+        <label for="obituary" class="form-label">obituary</label>
+        <input type="obituary" id = "text" class="form-control">
+    </div>
+    </div>
+    <div class="mt-3">
+        
+    </div>
+
+
+
+    
+</form>
+
+</div>
+
+</div>
+</body>
+</html>
