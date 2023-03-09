c8f8abb89d30d04834df45c59acb14dc33e0ff40
Adon13 <awoldema@emich.edu>
2023-03-08 19:58:45 -0500

    contact page initial layout

--- contact.php
+++ contact.php
@@ -1,33 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
 
- <a href="index.php" role="button" class = "btn btn-primary">Home</a>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <title>Contact</title>
+    </head>
 
-<div class="card w-50 mx-auto my-5">
-<div class="card-body">
-lawyer's contact info goes here
-    <div class="mt-3">
-       
-    </div>
-    <div class="mt-3">
+    <body style="height: 100vh; background-image: url('/public/img/graveyard-blur.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-size:cover;">
+
+        <div class="container-fluid text-center h-100;">
+            <div class="row">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <a href="login.php" role="button" class="btn btn-success ">log in</a>
+                    <a href="signup.php" role="button" class="btn btn-success">sign up</a>
+                    <a href="create_new.php" role="button" class="btn btn-success">add a new deceased</a>
+                    <a href="profile.php" role="button" class="btn btn-success">profile</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="card mx-auto my-5 rounded-4" style="max-width: 540px; background-color: rgba(179, 174, 173, 0.7);">
+            <div class="row g-0">
+                <div class="col-md-8">
+                    <img src="public/img/guy.JPG" class="img-fluid rounded-4" alt="picture of lawyer" style="width: 500px;">
+                </div>
+                <div class="col-md-4">
+                    <div class="card-body text-white">
+                        <h4 class="card-title pb-2">Saul McLawyer</h5>
+                        <p class="card-text pb-5">This is just a sample contact information text. Phone number and socials would go here. This content is a little bit longer.</p>
+                        <p class="card-footer pb-0 px-0 mx-0"><small class="text-muted">Number: (734) 734-7344</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
         
-    </div>
-
-
-
-    
-</form>
-
-</div>
-
-</div>
-</body>
+        
+        <!--
+            <a href="index.php" role="button" class = "btn btn-primary">Home</a>
+        -->
+    </body>
 </html>
