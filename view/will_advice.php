6a310c9ae65639f01404da294dfc35ad43015cec
Adon13 <awoldema@emich.edu>
2023-04-02 13:06:29 -0400

    filled out will advice

--- view/will_advice.php
+++ view/will_advice.php
@@ -2,24 +2,150 @@
     <?php include 'header.php';?>
     <!------------------------CODE STARTS------------------------------->
 
-        <div class="card w-50 mx-auto my-5">
-        <div class="card-body">
-        will advice info goes here
-            <div class="mt-3">
-            
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    What happens if I die without a will?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                <div class="accordion-body">
+                    <strong>If you die without a valid will, you'll become what's called intestate. </strong>That usually means your estate will be settled based on the laws of your state that outline who inherits what. Probate is the legal process of transferring the property of a deceased person to the rightful heirs.
+
+                    Since no executor was named, a judge appoints an administrator to serve in that capacity. An administrator also will be named if a will is deemed to be invalid. All wills must meet certain standards such as being witnessed to be legally valid. Again, requirements vary from state to state.
+
+                    An administrator will most likely be a stranger to you and your family, and he or she will be bound by the letter of the probate laws of your state. As such, an administrator may make decisions that wouldn't necessarily agree with your wishes or those of your heirs.                
+                </div>
             </div>
-            <div class="mt-3">
-                
-            </div>
-
-
-
-            
-        </form>
-
         </div>
 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                    Do I need an attorney to prepare my will?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                <div class="accordion-body">
+                    <strong>No, you aren't required to hire a lawyer to prepare your will, </strong>though an experienced lawyer can provide useful advice on estate-planning strategies such as living trusts. But as long as your will meets the legal requirements of your state, it's valid whether a lawyer drafted it or you wrote it yourself on the back of a napkin.
+
+                    Do-it-yourself will kits are widely available. Conduct an Internet search for "online wills" or "estate planning software" to find options, or check bookstores and libraries for will-writing guides. Your state's departments of aging also might be able to direct you to free or low-cost resources for estate planning.
+
+                    And while you're working on your will, you should think about preparing other essential estate-planning documents. "When you create or update your will, that's also a good time to think about other advance-planning tools like financial and health care powers of attorney to ensure that your wishes are carried out while you're still alive," says Naomi Karp of AARP's Public Policy Institute.
+                </div>
+            </div>
         </div>
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    Should my spouse and I have a joint will or separate wills?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                <div class="accordion-body">
+                    Estate planners almost universally advise against joint wills, and some states don't even recognize them. Odds are you and your spouse won't die at the same time, and there's probably property that's not jointly held. That's why separate wills make better sense, even though your will and your spouse's will might end up looking remarkably similar.
+
+                    In particular, separate wills allow for each spouse to address issues such as ex-spouses and children from previous relationships. Ditto for property that was obtained during a previous marriage. Be very clear about who gets what. Probate laws generally favor the current spouse.
+                </div>
+            </div>
+        </div>
+ 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingFour">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+                    Who should act as a witness to a will?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
+                <div class="accordion-body">
+                    Any person can act as a witness to your will, but you should select someone who isn't a beneficiary. Otherwise there's the potential for a conflict of interest. The technical term is a disinterested witness. Some states require two or more witnesses. If a lawyer drafts your will, he or she shouldn't serve as a witness.
+
+                    Not all states require a will to be notarized, but some do. Check. You may also want to have your witnesses sign what's called a self-proving affidavit in the presence of a notary. This affidavit can speed up the probate process because your witnesses likely won't be called into court by a judge to validate their signatures and the authenticity of the will.                
+                </div>
+            </div>
+        </div>
+ 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingFive">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
+                    Who should I name as my executor?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFive">
+                <div class="accordion-body">
+                    You can name your spouse, an adult child, or another trusted friend or relative as your executor. If your affairs are complicated, it might make more sense to name an attorney or someone with legal and financial expertise. You can also name joint executors, such as your spouse or partner and your attorney.
+
+                    One of the most important things your will can do is empower your executor to pay your bills and deal with debt collectors. Make sure the wording of your will allows for this, and also gives your executor leeway to take care of any related issues that aren't specifically outlined in your will.
+                </div>
+            </div>
+        </div>
+ 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingSix">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseSix" aria-expanded="false" aria-controls="panelsStayOpen-collapseSix">
+                    How do I leave specific items to specific heirs?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseSix" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingSix">
+                <div class="accordion-body">
+                    If you wish to leave certain personal property to certain heirs, indicate as much in your will. In addition, you can create a separate document called a letter of instruction that you should keep with your will.
+
+                    A letter of instruction, which isn't legally binding in some states, can be written more informally than a will and can go into detail about which items go to whom. You can also include specifics about any number of things that will help your executor settle your estate including account numbers, passwords and even burial instructions.
+
+                    Another option is to leave everything to one trusted person who knows your wishes for distributing your personal items. This, of course, is risky because you're relying on this person to honor your intentions without fail. Consider carefully.
+                </div>
+            </div>
+        </div>
+ 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingSeven">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseSeven" aria-expanded="false" aria-controls="panelsStayOpen-collapseSeven">
+                    Where should I keep my will?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseSeven" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingSeven">
+                <div class="accordion-body">
+                    A probate court usually requires your original will before it can process your estate, so it's important to keep the document safe yet accessible. If you put the will in a bank safe deposit box that only you can get into, your family might need to seek a court order to gain access. A waterproof and fireproof safe in your house is a good alternative.
+
+                    Your attorney or someone you trust should keep signed copies in case the original is destroyed. Signed copies can be used to establish your intentions. However, the absence of an original will can complicate matters, and without it there's no guarantee that your estate will be settled as you'd hoped.
+                </div>
+            </div>
+        </div>
+ 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingEight">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseEight" aria-expanded="false" aria-controls="panelsStayOpen-collapseEight">
+                    How often does a will need to be updated?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseEight" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingEight">
+                <div class="accordion-body">
+                    It's possible that your will may never need to be updated — or you may choose to update it regularly. The decision is yours. Remember, the only version of your will that matters is the most current valid one in existence at the time of your death.
+
+                    With that in mind, you may want to revisit your will at times of major life changes. Think of pivotal moments such as marriage, divorce, the birth of a child, the death of a beneficiary or executor, a significant purchase or inheritance, and so on. Your kids probably won't need guardians named in a will after they're adults, for example, but you might still need to name guardians for disabled dependents. A rule of thumb: Review your will every two or three years to be safe.
+                </div>
+            </div>
+        </div>
+ 
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingNine">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseNine" aria-expanded="false" aria-controls="panelsStayOpen-collapseNine">
+                    Who has the right to contest my will?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseNine" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingNine">
+                <div class="accordion-body">
+                    Contesting a will refers to challenging the legal validity of all or part of the document. A beneficiary who feels slighted by the terms of a will might choose to contest it. Depending on which state you live in, so too might a spouse, ex-spouse or child who believes your stated wishes go against local probate laws.
+
+                    A will can be contested for any number of other reasons: it wasn't properly witnessed; you weren't competent when you signed it; or it's the result of coercion or fraud. It's usually up to a probate judge to settle the dispute. The key to successfully contesting a will is finding legitimate legal fault with it. A clearly drafted and validly executed will is the best defense.
+                </div>
+            </div>
+        </div>
+ 
+    </div>
 
 <!------------------------CODE ENDS------------------------------->
 <?php include 'html_footer.php';?>
